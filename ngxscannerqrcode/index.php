ac59cab5231048692dafcc5faed112cbe45ef1ac
jcruz <jcruz@gruporivero.com>
2024-04-26 19:25:23 -0600

    bar code test

--- ngxscannerqrcode/index.php
+++ ngxscannerqrcode/index.php
@@ -10,43 +10,33 @@
 
 <body>
     <div style="width: 500px" id="reader"></div>
-
+    <pre style="height: 30px; border: 2px gray solid;" id="log"></pre>
 </body>
 <script src="html5-qrcode.min.js"></script>
 
 <script>
-    function onScanSuccess(decodedText, decodedResult) {
-        // Handle on success condition with the decoded text or result.
-        console.log(`Scan result: ${decodedText}`, decodedResult);
+
+(function () {
+    var old = console.log;
+    var logger = document.getElementById('log');
+    console.log = function (message) {
+        if (typeof message == 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+        } else {
+            logger.innerHTML += message + '<br />';
+        }
     }
+})();
+setTimeout(() => {
+    console.log('helll');
+}, 1000);
 
-    var html5QrcodeScanner = new Html5QrcodeScanner(
-        "reader", {
-            fps: 10,
-            qrbox: 250
-        });
-    html5QrcodeScanner.render(onScanSuccess);
-
-    var html5QrcodeScanner = new Html5QrcodeScanner(
-        "reader", {
-            fps: 10,
-            qrbox: 250
-        });
 
     function onScanSuccess(decodedText, decodedResult) {
         // Handle on success condition with the decoded text or result.
         console.log(`Scan result: ${decodedText}`, decodedResult);
+        alert(decodedResult);
         alert(decodedText);
-        // ...
-        html5QrcodeScanner.clear();
-        // ^ this will stop the scanner (video feed) and clear the scan area.
-    }
-
-    html5QrcodeScanner.render(onScanSuccess);
-
-    function onScanSuccess(decodedText, decodedResult) {
-        // Handle on success condition with the decoded text or result.
-        console.log(`Scan result: ${decodedText}`, decodedResult);
     }
 
     function onScanError(errorMessage) {
