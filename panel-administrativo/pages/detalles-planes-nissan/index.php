b72300e3b8d339e22c723770056bf7e440e4c042
GabrielValles8 <gabrielvalles8a@gmail.com>
2024-08-14 15:48:04 -0600

    3.5

--- panel-administrativo/pages/detalles-planes-nissan/index.php
+++ panel-administrativo/pages/detalles-planes-nissan/index.php
@@ -16,13 +16,17 @@ if ($resultQuery->num_rows > 0) {
     }
 }
 
-
-
 //echo $auto[0]["modelo"];
 
-$versiones = $conne->get_lista_versiones_nissan($auto["modelo"], $auto["ano"]);
-var_dump($versiones);
-foreach ($versiones as $key => $value) {
+$sqlVersiones = $conne->get_lista_versiones_nissan($auto["modelo"], $auto["ano"]);
+$versiones = $conn->query($sqlVersiones);
+if ($versiones->num_rows > 0) {
+    while ($row = $versiones->fetch_assoc()) {
+        $vers = $row;
+    }
+}
+var_dump($vers);
+foreach ($vers as $key => $value) {
     $params = base64_encode(json_encode($value));
     $lista_versiones.='<div class="card" >' +
                         '<div class="card-body" >' +
@@ -33,9 +37,110 @@ foreach ($versiones as $key => $value) {
                             '<input class="form-control" style="width: 100%" type="text" id="mensualidad_'.$value["version"].'" hidden>'+
                             '<h5 style="display: flex; align-items: center; justify-content: center;" class="card-title">PRECIO CONTADO: '.$value["precio"].'</h5>' +
                             '<input class="form-control" style="width: 100%" type="text" id="precio_'.$value["version"].'" value="'.$value["precio"].'" hidden>'+
-/*                             '<a onclick="modalEditar(\''.$value["version"].'\')" style="display: flex; align-items: center; justify-content: center;" class="btn btn-primary" >Editar </a>' + */
+                            '<a onclick="modalEditar(\''.$value["version"].'\')" style="display: flex; align-items: center; justify-content: center;" class="btn btn-primary" >Editar </a>' +
                         '</div>' +
                     '</div>';
 }
 ?>
 
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <?php include('../../_inc/_header.php'); ?>
+    <link rel='icon' type='image/png' href='https://www.gruporivero.com/assets/img/commun/gporiv.png' />
+    <!--link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/-->
+    <link rel='shortcut icon' type='image/png' href='https://www.gruporivero.com/assets/img/commun/gporiv.png' />
+
+    <!-- JQuery DataTable Css -->
+    <link href="../../plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- Bootstrap Core Css -->
+    <link href="../../plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- JQuery DataTable Css -->
+    <link href="../../plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- Custom Css -->
+    <link href="../../css/style.css" rel="stylesheet">
+
+    <!-- Sweetalert Css -->
+    <link href="../../plugins/sweetalert/sweetalert.css" rel="stylesheet" />
+
+    <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
+    <link href="../../css/themes/all-themes.css" rel="stylesheet" />
+
+    <!-- Bootstrap Select Css -->
+    <!-- <link href="../../plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet"> -->
+
+
+</head>
+
+<body class="theme-blue">
+    <!-- #Top Bar -->
+    <?php include('../../_inc/_search-bar.php'); ?>
+    <!-- #Menu -->
+    <section>
+        <?php include('../../_inc/_menu.php'); ?>
+        <!-- #END# Left Sidebar -->
+        <!-- Right Sidebar -->
+        <? //include('../../_inc/_gadgets.php');
+        ?>
+        <!-- #END# Right Sidebar -->
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <hr/>
+            <div>
+                <?= $lista_versiones; ?>
+            </div>
+        </div>
+    </section>
+
+        <!-- Jquery Core Js -->
+        <script src="../../plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+
+<!-- Select Plugin Js -->
+<!-- <script src="../../plugins/bootstrap-select/js/bootstrap-select.js"></script> -->
+
+<!-- Dropzone Plugin Js 
+<script src="../../plugins/dropzone/dropzone.js"></script>
+-->
+<!-- Jquery Spinner Plugin Js -->
+<script src="../../plugins/jquery-spinner/js/jquery.spinner.js"></script>
+
+<!-- Bootstrap Tags Input Plugin Js 
+<script src="../../plugins/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
+-->
+<!-- Waves Effect Plugin Js -->
+<script src="../../plugins/node-waves/waves.js"></script>
+
+<!-- SweetAlert Plugin Js -->
+<script src="../../plugins/sweetalert/sweetalert.min.js"></script>
+
+<!-- Custom Js -->
+<script src="../../js/admin.js"></script>
+
+<!-- Jquery DataTable Plugin Js -->
+<script src="../../plugins/jquery-datatable/jquery.dataTables.js"></script>
+<script src="../../plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+<!-- Custom Js -->
+<!-- <script src="../../js/admin.js"></script> -->
+<script src="../../js/pages/tables/jquery-datatable.js"></script>
+
+</body>
+
+</html>
