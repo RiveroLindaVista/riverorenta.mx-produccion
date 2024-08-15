07e72522cb7da1981213e4053e4d59af10eb74e8
GabrielValles8 <gabrielvalles8a@gmail.com>
2024-08-15 09:16:49 -0600

    7.4

--- panel-administrativo/pages/detalles-planes-nissan/update_plan.php
+++ panel-administrativo/pages/detalles-planes-nissan/update_plan.php
@@ -3,18 +3,19 @@ include("../../_inc/conexion.php");
 include("../../_inc/consultas.php");
 require_once("../../_inc/_config.php");
 
-$conn=new Conexion();
+$conne=new Conexion();
 //var_dump($_POST);
 
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DB);
 $sql = 'SELECT * FROM planes_nissan WHERE modelo="'.$_POST["modelo"].'" and version="'.$_POST["version"].'" and ano="'.$_POST["ano"].'"';
 
 $result=$conn->query($sql);
-/* if ($result) {
+if ($result) {
     while ($row = $result->fetch_assoc()) {
         $out[]=$row;
     }
     echo json_encode($out);
-} */
+}
 
 /*     $query = "INSERT INTO accesorios (auto,num_inventario,descripcion,tiempo_instalacion,precio,status,instalacion,categoria) VALUES('$ano','$modelo','$tecnologia','$tipo')";
 	$conn->query($query); */
