95a70d9c1d48a60152f4bd66a089280f57a753fd
GabrielValles8 <gabrielvalles8a@gmail.com>
2024-08-15 07:30:44 -0600

    6.7

--- panel-administrativo/pages/detalles-planes-nissan/index.php
+++ panel-administrativo/pages/detalles-planes-nissan/index.php
@@ -104,3 +104,49 @@ print_r($lista_versiones);
             </div>
         </div>
     </section>
+
+        <!-- Jquery Core Js -->
+        <script src="../../plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core Js -->
+<script src="../../plugins/bootstrap/js/bootstrap.js"></script>
+
+<!-- Select Plugin Js -->
+<!-- <script src="../../plugins/bootstrap-select/js/bootstrap-select.js"></script> -->
+
+<!-- Dropzone Plugin Js 
+<script src="../../plugins/dropzone/dropzone.js"></script>
+-->
+<!-- Jquery Spinner Plugin Js -->
+<script src="../../plugins/jquery-spinner/js/jquery.spinner.js"></script>
+
+<!-- Bootstrap Tags Input Plugin Js 
+<script src="../../plugins/bootstrap-tagsinput/bootstrap-tagsinput.js"></script>
+-->
+<!-- Waves Effect Plugin Js -->
+<script src="../../plugins/node-waves/waves.js"></script>
+
+<!-- SweetAlert Plugin Js -->
+<script src="../../plugins/sweetalert/sweetalert.min.js"></script>
+
+<!-- Custom Js -->
+<script src="../../js/admin.js"></script>
+
+<!-- Jquery DataTable Plugin Js -->
+<script src="../../plugins/jquery-datatable/jquery.dataTables.js"></script>
+<script src="../../plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+<script src="../../plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
+
+<!-- Custom Js -->
+<!-- <script src="../../js/admin.js"></script> -->
+<script src="../../js/pages/tables/jquery-datatable.js"></script>
+
+</body>
+
+</html>
