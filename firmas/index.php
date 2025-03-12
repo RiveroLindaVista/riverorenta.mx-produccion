cf5ec3063dbb5e02f908559b854b76d7b3cb9ec6
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-12 10:29:38 -0600

    Prueba Chat Nissan

--- firmas/index.php
+++ firmas/index.php
@@ -7,5 +7,8 @@
 </head>
 <body>
     <h2>helllo world222222</h2>
+    <script type="module" src="https://platform.seez.co/sdk/seez-sdk.js" data-seez-client-id="nissanriveromx"></script>
+    <seez-sdk-seezar-modal></seez-sdk-seezar-modal>
+    <script src="https://govirtual.la/seezar/seezarmx-functions.js"></script>
 </body>
 </html>
