a82d54a20b7cefbac06ef128b57212fed3f3798f
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-29 11:18:34 -0600

    Conexion

--- nissan/valua-tu-auto/consultas.php
+++ nissan/valua-tu-auto/consultas.php
@@ -2,9 +2,9 @@
 include_once("conexion.php");
 
 class Conexion extends Database{
-    public function query_autometrica(){
+    public function get_marcas($ano){
         $conn= Database::connect();
-        $sql = "SELECT * FROM valuacion_autometrica limit 10";
+        $sql = "SELECT marca FROM valuacion_autometrica WHERE year =".$ano." group by marca, order by marca asc ";
         $result=$conn->query($sql);
         if ($result) {
                     while ($row = $result->fetch_assoc()) {
--- nissan/valua-tu-auto/getMarcas.php
+++ nissan/valua-tu-auto/getMarcas.php
@@ -1,14 +1,9 @@
 <?php
-include("_config.php");
-$conn = new mysqli(DB_HOST, DB_USER,DB_PASSWORD, DB_DB);
-echo $_POST;
-$sql = "Select marca from valuacion_autometrica WHERE year =".$_POST["ano"]." group by marca order by marca asc";
-$resultQuery = $conn->query($sql);
-if ($resultQuery->num_rows > 0) {
-    while($row = $resultQuery->fetch_assoc()) {
-        $opcionesMarcas.='<option value="'.$row['marca'].'">'. $row['marca'].'</option>';
-    }
-}
+require_once("../../_inc/_config.php");
+include("../../_inc/constructor.php");
 
-return $opcionesMarcas;
+$ano=$_POST['ano'];
+$conne = new Conexion();
+$marcas = $conne->get_marcas($ano);
+print_r($marcas);
 ?>
