4890e514aa341479062f31252c47c41e325f5f47
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-29 11:40:49 -0600

    14

--- nissan/valua-tu-auto/constructor.php
+++ nissan/valua-tu-auto/constructor.php
@@ -4,9 +4,10 @@ include_once("consultas.php");
 session_start();
 class Construir extends Conexion{
    public function get_marcas($ano){
-    return 'PIPI';
+
       $conn=new Conexion();
       $consulta= $conn->query_marcas($ano);
+      return 'PIPI';
       if ($consulta) {
         for($i=0;$i<count($consulta);$i++){
             $cadena.='<option selected value="'.$consulta[$i].'">'.$consulta[$i].'</option>';
