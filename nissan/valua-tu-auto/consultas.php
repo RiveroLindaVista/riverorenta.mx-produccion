6c04c7869676789adc6df8fa83cb59951ae34fbb
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-29 11:44:16 -0600

    18

--- nissan/valua-tu-auto/consultas.php
+++ nissan/valua-tu-auto/consultas.php
@@ -5,14 +5,14 @@ class Conexion extends Database{
     public function query_marcas($ano){
         
         $conn= Database::connect();
-        $sql = "SELECT marca FROM valuacion_autometrica WHERE year =".$ano." group by marca, order by marca asc ";
+        $sql = "SELECT marca FROM valuacion_autometrica WHERE year =".$ano." group by marca";
         $result=$conn->query($sql);
         if ($result) {
                     while ($row = $result->fetch_assoc()) {
                         $out[]=$row;
                     }
                 } 
-        return $sql;
+        return $out;
         $conn=Database::close();
     }
 }
