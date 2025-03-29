dc2b68316c053da9d6029b6604784c442df8f30d
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-29 11:35:11 -0600

    8

---
+++ nissan/valua-tu-auto/constructor.php
@@ -0,0 +1,16 @@
+<?php
+include_once("consultas.php");
+
+session_start();
+class Construir extends Conexion{
+   public function get_marcas($ano){
+      $conn=new Conexion();
+      $consulta= $conn->query_marcas($ano);
+      if ($consulta) {
+        for($i=0;$i<count($consulta);$i++){
+            $cadena.='<option selected value="'.$consulta[$i].'">'.$consulta[$i].'</option>';
+        }
+      }
+      return  $cadena;
+   }
+}
--- nissan/valua-tu-auto/getMarcas.php
+++ nissan/valua-tu-auto/getMarcas.php
@@ -2,14 +2,7 @@
 include("_config.php");
 
 $ano=$_POST['ano'];
-$conn = new mysqli(DB_HOST, DB_USER,DB_PASSWORD, DB_DB);
-$sql = "SELECT marca FROM valuacion_autometrica WHERE year =".$ano." group by marca, order by marca asc ";
-$resultQuery = $conn->query($sql);
-if ($resultQuery->num_rows > 0) {
-    while($row = $resultQuery->fetch_assoc()) {
-        echo $row['marca'];
-        $opcionesMarcas.='<option value="'.$row['marca'].'">'. $row['marca'].'</option>';
-    }
-}
-return $sql;
+$conne = new Construir();
+$marcas = $conne->get_marcas($ano);
+print_r($marcas);
 ?>
