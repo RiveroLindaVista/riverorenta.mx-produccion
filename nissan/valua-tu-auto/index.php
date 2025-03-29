e8500075a3918ac9bf9b5250f66d740cea0eb5cf
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-29 10:17:27 -0600

    Valua4

--- nissan/valua-tu-auto/index.php
+++ nissan/valua-tu-auto/index.php
@@ -2,11 +2,17 @@
 include("_config.php");
 echo 'HOLA';
 $conn = new mysqli(DB_HOST, DB_USER,DB_PASSWORD, DB_DB);
-$sql = "Select year from valuacion_autometrica group by year";
+$sql = "Select year from valuacion_autometrica group by year order by year desc";
 $resultQuery = $conn->query($sql);
 if ($resultQuery->num_rows > 0) {
     while($row = $resultQuery->fetch_assoc()) {
+        $opcionesYears.='<option value="'.$row['year'].'">'. $row['year'].'</option>';
         echo $row['year'];
     }
  }
  ?>
+
+ <h1>Cuéntanos sobre tu auto</h1>
+ <select class="form-control" id="filtroYears">
+        <?=$opcionesYears?>
+    </select>
