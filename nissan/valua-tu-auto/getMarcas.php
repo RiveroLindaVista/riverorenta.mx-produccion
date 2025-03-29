e87fc6cecbfa206d63804697f86129b8fbb28b7f
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-29 11:25:32 -0600

    4

--- nissan/valua-tu-auto/getMarcas.php
+++ nissan/valua-tu-auto/getMarcas.php
@@ -3,6 +3,12 @@ require_once("_config.php");
 
 $ano=$_POST['ano'];
 $conn = new mysqli(DB_HOST, DB_USER,DB_PASSWORD, DB_DB);
-$marcas = $conn->get_marcas($ano);
-print_r($marcas);
+$sql = "SELECT marca FROM valuacion_autometrica WHERE year =".$ano." group by marca, order by marca asc ";
+$resultQuery = $conn->query($sql);
+if ($resultQuery->num_rows > 0) {
+    while($row = $resultQuery->fetch_assoc()) {
+        $opcionesMarcas.='<option value="'.$row['year'].'">'. $row['year'].'</option>';
+    }
+}
+echo $opcionesMarcas;
 ?>
