7d457379b38ee2cb48fac9e3cce9136dd793d12a
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-29 10:23:00 -0600

    Valua6

--- nissan/valua-tu-auto/index.php
+++ nissan/valua-tu-auto/index.php
@@ -1,4 +1,36 @@
 <?php
-//include("_config.php");
+include("_config.php");
 echo 'HOLA';
+$conn = new mysqli(DB_HOST, DB_USER,DB_PASSWORD, DB_DB);
+$sql = "Select year from valuacion_autometrica group by year order by year desc";
+$resultQuery = $conn->query($sql);
+if ($resultQuery->num_rows > 0) {
+    while($row = $resultQuery->fetch_assoc()) {
+        $opcionesYears.='<option value="'.$row['year'].'">'. $row['year'].'</option>';
+        echo $row['year'];
+    }
+ }
+ ?>
 
+<!doctype html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Valua tu carro - Nissan Rivero</title>
+        
+    <link href="<?=URL?>/estilos/main.css" rel="stylesheet" type="text/css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    </head>
+
+    <body>
+
+        <h1>Cuéntanos sobre tu auto</h1>
+        <select class="form-control" id="filtroYears">
+            <?=$opcionesYears?>
+        </select>
+    </body>
+    
+</html>
