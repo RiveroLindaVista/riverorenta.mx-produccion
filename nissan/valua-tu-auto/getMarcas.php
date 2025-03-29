c1179e88810e869348ec129fb23345f29aeafe7b
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-29 11:19:17 -0600

    Test post

--- nissan/valua-tu-auto/getMarcas.php
+++ nissan/valua-tu-auto/getMarcas.php
@@ -3,7 +3,5 @@ require_once("../../_inc/_config.php");
 include("../../_inc/constructor.php");
 
 $ano=$_POST['ano'];
-$conne = new Conexion();
-$marcas = $conne->get_marcas($ano);
-print_r($marcas);
+echo $ano;
 ?>
