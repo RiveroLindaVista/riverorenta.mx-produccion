570ff96d39470496e2ab1fccbb38158e00ae1407
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-31 11:40:40 -0600

    21

--- nissan/valua-tu-auto/index.php
+++ nissan/valua-tu-auto/index.php
@@ -206,8 +206,12 @@ if ($marcasQry->num_rows > 0) {
 
         fetch("https://multimarca.gruporivero.com/api/v1/autometrica/lineal?empresa=nissan&year="+select_ano+"&brand="+select_marca+"&subbrand="+select_modelo+"&version="+select_version+"&kilometraje="+select_km+"", requestOptions)
         .then((response) => response.text())
-        .then((result) => console.log(JSON.parse(result)))
+        .then((result) => this.objetoOferta(JSON.parse(result)))
         .catch((error) => console.error(error));
+    }
+
+    function objetoOferta(obj){
+        console.log('KAKAKAK');
     }
 </script>
 
