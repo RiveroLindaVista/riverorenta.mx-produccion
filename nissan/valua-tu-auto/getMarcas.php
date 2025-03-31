b8bcfd43002513997cf93432584dc3becaba6fb8
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-03-31 07:42:17 -0600

    35

--- nissan/valua-tu-auto/getMarcas.php
+++ nissan/valua-tu-auto/getMarcas.php
@@ -5,7 +5,7 @@ include("constructor.php");
 $ano=$_POST['ano'];
 $conne = new Construir();
 $marcas = $conne->get_marcas($ano);
-
+$marcas = json_encode($marcas);
 for($i=0;$i<count($marcas);$i++){
     $cadena.='<option selected value="'.$marcas[$i].'">'.$marcas[$i].'</option>';
 }
