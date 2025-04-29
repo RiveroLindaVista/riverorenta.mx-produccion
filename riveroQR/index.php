528a1ced1b818bd62edf32f00f7b3242d7f2f8d8
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-04-29 10:32:12 -0600

    Proyecto QR Rivero

---
+++ riveroQR/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Generador de QR Grupo Rivero</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+</head>
+<body>
+  <div id="qrcode" style="display: flex;justify-content: center;"></div>
+
+  <script>
+    // URL de destino (puedes construirla dinámicamente)
+    const params = new URLSearchParams(window.location.search);
+    const paginaSplit = window.location.href.split("?pagina=");
+    
+    console.log(paginaSplit[1]);
+    const usuario = "juan";
+    const accion = "ver";
+    const pagina = paginaSplit[1];
+    
+    const url = `${pagina}&usuario=${usuario}&accion=${accion}`;
+
+    //const url = `http://www.google.com/calendar/event?action=TEMPLATE&dates=20251031T110000/20251031T120000&text=Taller+Cita+de+Servicio&location=Av.Miguel+Aleman+5400&details=Cita+en+taller+de+servicio+para+tu+Aveo+2020+en+Rivero+Linda+Vista&usuario=${usuario}&accion=${accion}`;
+   // http://www.google.com/calendar/event?action=TEMPLATE&dates=20201031T11:00:00-01:00/20201031T12:00:00-01:00&text=Taller+Cita+de+Servicio&location=Av.Miguel+Alemnán+5400&details=Cita+en+taller+de+servicio+para+tu+Aveo+2020+en+Rivero+Linda+Vista
+    // Generar el QR
+    new QRCode(document.getElementById("qrcode"), {
+      text: url,
+      width: 170,
+      height: 170
+    });
+  </script>
+
+</body>
+</html>
