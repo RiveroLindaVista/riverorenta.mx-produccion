7a419dd8fe020123c562268826a32606df146f8c
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-04-29 08:47:41 -0600

    2.2

--- firmas2/index.php
+++ firmas2/index.php
@@ -16,9 +16,9 @@
     const accion = "ver";
     const pagina = $_POST["pagina"];
     
-    const url = `${pagina}&usuario=${usuario}&accion=${accion}`;
+    //const url = `${pagina}&usuario=${usuario}&accion=${accion}`;
 
-    //const url = `http://www.google.com/calendar/event?action=TEMPLATE&dates=20251031T110000/20251031T120000&text=Taller+Cita+de+Servicio&location=Av.Miguel+Aleman+5400&details=Cita+en+taller+de+servicio+para+tu+Aveo+2020+en+Rivero+Linda+Vista&usuario=${usuario}&accion=${accion}`;
+    const url = `http://www.google.com/calendar/event?action=TEMPLATE&dates=20251031T110000/20251031T120000&text=Taller+Cita+de+Servicio&location=Av.Miguel+Aleman+5400&details=Cita+en+taller+de+servicio+para+tu+Aveo+2020+en+Rivero+Linda+Vista&usuario=${usuario}&accion=${accion}`;
    // http://www.google.com/calendar/event?action=TEMPLATE&dates=20201031T11:00:00-01:00/20201031T12:00:00-01:00&text=Taller+Cita+de+Servicio&location=Av.Miguel+Alemnán+5400&details=Cita+en+taller+de+servicio+para+tu+Aveo+2020+en+Rivero+Linda+Vista
     // Generar el QR
     new QRCode(document.getElementById("qrcode"), {
