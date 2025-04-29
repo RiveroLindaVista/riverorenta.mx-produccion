d59fcc81b260d7d586979717e65ed9b05440f816
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-04-29 08:59:29 -0600

    2.6

--- firmas2/index.php
+++ firmas2/index.php
@@ -13,14 +13,16 @@
   <script>
     // URL de destino (puedes construirla dinámicamente)
     const params = new URLSearchParams(window.location.search);
-    console.log(window.location);
+    const paginaSplit = window.location.href.split("?pagina=");
+    
+    console.log(paginaSplit[1]);
     const usuario = "juan";
     const accion = "ver";
-    const pagina = params.get('pagina');
+    const pagina = paginaSplit[1];
     //const pagina = $_POST["pagina"];
     
     const url = `${pagina}&usuario=${usuario}&accion=${accion}`;
-
+    console.log(url);
     //const url = `http://www.google.com/calendar/event?action=TEMPLATE&dates=20251031T110000/20251031T120000&text=Taller+Cita+de+Servicio&location=Av.Miguel+Aleman+5400&details=Cita+en+taller+de+servicio+para+tu+Aveo+2020+en+Rivero+Linda+Vista&usuario=${usuario}&accion=${accion}`;
    // http://www.google.com/calendar/event?action=TEMPLATE&dates=20201031T11:00:00-01:00/20201031T12:00:00-01:00&text=Taller+Cita+de+Servicio&location=Av.Miguel+Alemnán+5400&details=Cita+en+taller+de+servicio+para+tu+Aveo+2020+en+Rivero+Linda+Vista
     // Generar el QR
