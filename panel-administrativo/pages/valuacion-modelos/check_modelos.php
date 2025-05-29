e02363bcc5f98752b29b2674bb3f4caef604f704
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-05-29 12:42:47 -0600

    2.8

--- panel-administrativo/pages/valuacion-modelos/check_modelos.php
+++ panel-administrativo/pages/valuacion-modelos/check_modelos.php
@@ -8,5 +8,5 @@ $ano=$_POST['ano'];
 $tipo=$_POST['tipo'];
 $conne = new Construir();
 $modelos = $conne->check_modelos_valuacion($marca,$modelo,$ano,$tipo);
-print_r($modelos);
+echo json_encode($modelos);
 ?>
