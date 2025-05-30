c00b71fc2cfd7e34e0aaaa48e298238a83ab7757
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-05-30 10:21:41 -0600

    4.6

--- panel-administrativo/_inc/consultas.php
+++ panel-administrativo/_inc/consultas.php
@@ -1909,9 +1909,9 @@ class Conexion extends Database{
             if($out == null){
                 $sqlInsert = "INSERT INTO autometrica_modelos (marca,modelo,ano,tipo) VALUES('".$marca."','".$modelo."','".$ano."','".$tipo."')";
                 $resultInsert=$conn->query($sqlInsert);
-                return '0';
+                return 0;
             } else {
-                return '1';
+                return 1;
             }
         }
     }
