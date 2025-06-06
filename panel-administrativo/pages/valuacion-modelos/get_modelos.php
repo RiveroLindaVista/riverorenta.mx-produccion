a224fa85f16cee4bafad5e6d8894d86aede73012
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-06-06 09:49:55 -0600

    1.0

--- panel-administrativo/pages/valuacion-modelos/get_modelos.php
+++ panel-administrativo/pages/valuacion-modelos/get_modelos.php
@@ -7,5 +7,5 @@ $modelo=$_POST['modelo'];
 $id=$_POST['id'];
 $conne = new Construir();
 $modelos = $conne->get_modelos_valuacion($id,$marca,$modelo);
-print_r($modelos);
+print_r(utf8_encode($modelos));
 ?>
