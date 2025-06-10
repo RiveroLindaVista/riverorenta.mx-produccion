67ce48b01051c0d37e79882e9452a3b47253f51b
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-06-10 09:31:59 -0600

    3.3

--- panel-administrativo/pages/valuacion-modelos/index.php
+++ panel-administrativo/pages/valuacion-modelos/index.php
@@ -175,7 +175,9 @@ $conn->close();
                             <div class="modal-body">
                                 <div>
                                     <div id="tabla_faltantes" style="text-align:center">
-
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
