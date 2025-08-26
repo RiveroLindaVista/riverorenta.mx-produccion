d06c44113500510d59e482b351fe85caea7fe39c
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-08-26 17:00:27 -0600

    1.0

--- chevrolet/valua-tu-auto-chevrolet/index.php
+++ chevrolet/valua-tu-auto-chevrolet/index.php
@@ -29,7 +29,7 @@ if ($marcasQry->num_rows > 0) {
 <html>
     <head>
     <meta charset="UTF-8">
-    <title>Valua tu carro - Nissan Rivero</title>
+    <title>Valua tu carro - Chevrolet Rivero</title>
         
     <link href="<?=URL?>/estilos/main.css" rel="stylesheet" type="text/css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
