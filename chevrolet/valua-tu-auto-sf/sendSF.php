329643fc5fbcba5cd4f9df0021e7535db43c4a06
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-12-04 11:23:43 -0600

    Test secret

---
+++ chevrolet/valua-tu-auto-sf/sendSF.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+//$tipoSend='test';
+$tipoSend='prod';
+$urlDest='crear-Valuacion';
+
+echo getenv();
+
+
+
+?>
