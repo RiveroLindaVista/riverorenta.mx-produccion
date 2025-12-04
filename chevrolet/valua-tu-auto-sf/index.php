63d6e1b6a2b3ac417f8b08aa4bcfbc179f7e3fe4
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-12-04 12:23:23 -0600

    1.8

--- chevrolet/valua-tu-auto-sf/index.php
+++ chevrolet/valua-tu-auto-sf/index.php
@@ -511,11 +511,11 @@ if ($marcasQry->num_rows > 0) {
         
         $.ajax({
             type: "POST",
-            url: "sendSF.php",
+            url: "https://riverorenta.mx/api/salesforce/valuacion-express-sf/resumen/send-salesforce.php",
             data: data,
             dataType: "json",
             success: function(resp) {
-                console.log('Entramaaas');
+                console.log('Entra SF', resp);
             }
         });
 
