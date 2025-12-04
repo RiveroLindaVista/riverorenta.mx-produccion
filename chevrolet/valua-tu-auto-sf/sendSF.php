af9dff7136dc19c60dd003a3a76230430b0e06c3
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-12-04 12:02:50 -0600

    1.3

--- chevrolet/valua-tu-auto-sf/sendSF.php
+++ chevrolet/valua-tu-auto-sf/sendSF.php
@@ -8,6 +8,22 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-print_r($_SERVER);
+//$tipoSend='test';
+$tipoSend='prod';
+$urlDest='crear-Valuacion';
+
+	$ownerid=$_POST["ownerid"];
+	$leadid=$_POST["leadid"];
+	$opid=$_POST["opid"];
+	$km=$_POST["km"];
+	$version=$_POST["version"];
+	$marca=$_POST["marca"];
+	$ano=$_POST["ano"];
+	$modelo=$_POST["modelo"];
+	$ofrecido=$_POST["ofrecido"];
+	$venta=$_POST["venta"];
+	$compra=$_POST["compra"];
+
+    echo $ownerid;
 
 ?>
