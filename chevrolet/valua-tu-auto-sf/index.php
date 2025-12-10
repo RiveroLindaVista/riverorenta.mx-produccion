aed223f189f0ea48f41d09675502c26c0710dfb9
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-12-10 16:59:51 -0600

    1.2

--- chevrolet/valua-tu-auto-sf/index.php
+++ chevrolet/valua-tu-auto-sf/index.php
@@ -394,7 +394,7 @@ if ($marcasQry->num_rows > 0) {
 
         console.log("EIT: ",`https://multimarca.gruporivero.com/api/v1/autometrica/lineal?empresa=nissan&year=${select_ano}&brand=${select_marca}&subbrand${select_modelo}&version=${select_version}&kilometraje=${select_km}`);
 
-            fetch(`https://multimarca.gruporivero.com/api/v1/autometrica/lineal?empresa=chevrolet&year=${select_ano}&brand=${select_marca}&subbrand${select_modelo}&version=${select_version}&kilometraje=${select_km}`, requestOptions)
+            fetch(`https://multimarca.gruporivero.com/api/v1/autometrica/lineal?empresa=chevrolet&year=${select_ano}&brand=${select_marca}&subbrand=${select_modelo}&version=${select_version}&kilometraje=${select_km}`, requestOptions)
             .then((response) => response.text())
             .then((result) => this.objetoOferta(JSON.parse(result)))
             .catch((error) => console.error(error));
