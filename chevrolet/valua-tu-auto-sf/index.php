c8715fd6de192bc59bc49fb92427b1ab582e40f1
GabrielValles8 <gabrielvalles8a@gmail.com>
2025-12-10 17:17:57 -0600

    1.5

--- chevrolet/valua-tu-auto-sf/index.php
+++ chevrolet/valua-tu-auto-sf/index.php
@@ -384,17 +384,19 @@ if ($marcasQry->num_rows > 0) {
         let select_version = $('#filtroVersiones').val();
         let select_km = $('#filtroKM').val();
 
+        console.log("33    ",select_version);
         select_version = select_version.replaceAll(" ", "%20");
-        select_version = select_version.replaceAll('"', '%22');
+        select_version = select_version.replaceAll('\"', '%22');
         select_modelo = select_modelo.replaceAll(" ", "%20");
 
+        console.log(select_version, " - EIT: ",`https://multimarca.gruporivero.com/api/v1/autometrica/lineal?empresa=nissan&year=${select_ano}&brand=${select_marca}&subbrand=${select_modelo}&version=${select_version}&kilometraje=${select_km}`);
 
 /*         let obj = '{"lineal": [{"year": 2024,"brand": "Chevrolet","subbrand": "Onix","version": "4 pts. LS, 1.3l, TM5, a\/ac., BA, R-15","km_group": "A","sale": 239000,"purchase": 209800},{"year": 2024,"brand": "Chevrolet","subbrand": "Onix","version": "Valor kilometraje","km_group": "A","sale": -4800,"purchase": -4800}]}';
         objetoOferta(JSON.parse(obj)); */
         const requestOptions = { 
         };
 
-        console.log(select_version, " - EIT: ",`https://multimarca.gruporivero.com/api/v1/autometrica/lineal?empresa=nissan&year=${select_ano}&brand=${select_marca}&subbrand${select_modelo}&version=${select_version}&kilometraje=${select_km}`);
+        
 
             fetch(`https://multimarca.gruporivero.com/api/v1/autometrica/lineal?empresa=chevrolet&year=${select_ano}&brand=${select_marca}&subbrand=${select_modelo}&version=${select_version}&kilometraje=${select_km}`, requestOptions)
             .then((response) => response.text())
