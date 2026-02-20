b0f9b8bd0ceb0d61a9dcb18f291a0f4a86f1d979
GabrielValles8 <gabrielvalles8a@gmail.com>
2026-02-20 10:26:03 -0600

    4.0

--- nissan/valua-tu-auto-sf-beta/index.php
+++ nissan/valua-tu-auto-sf-beta/index.php
@@ -621,8 +621,9 @@ console.log("Entro al cambio: ", ofertas.precio_ofrecido);
         $(".extra-option:checked").each(function() {
             totalExtras += parseFloat($(this).val());
         });
-console.log("Entro al cambio2222: ", ofertas.precio_ofrecido);
+
         ofertas.precio_ofrecido = precio_ofrecido + totalExtras;
+        console.log("Entro al cambio2222: ", ofertas.precio_ofrecido, " + ", totalExtras);
 
         $("#precio").html('$ '+new Intl.NumberFormat('en-US').format(ofertas.precio_ofrecido)+'.00 MXN');
     });
